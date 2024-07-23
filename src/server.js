ad35cc092b5f1417a369415b5fbfd64213609100
MNO-proA <michaelnanaofosuofficial@gmail.com>
2024-07-23 08:48:18 +0000

    still on cors

--- src/server.js
+++ src/server.js
@@ -3,17 +3,27 @@ const express = require('express');
 const dotenv = require('dotenv');
 const routes = require('./routes');
 const cors = require('cors');
+const helmet = require('helmet');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+// Use Helmet to set various HTTP headers for security
+app.use(helmet());
 
+// Customize Helmet's cross-origin resource policy
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+// Configure CORS to allow requests from your frontend's origin
 app.use(cors({
-    origin: 'https://jta-aws-frontend.onrender.com',
+    origin: 'https://jta-aws-frontend.onrender.com', // Replace with your frontend's URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     optionsSuccessStatus: 200
 }));
+
 app.use('/', routes);
 
 // Only start the server if we're not in a Lambda environment
